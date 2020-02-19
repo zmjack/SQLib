613a6ee5bcdb13e7cf05c5aaf31b28edfc8b135b
zmjack <jack@zmland.com>
2020-02-20 04:51:02 +0800

    v0.2.0

---
+++ SqlPlusApp/Data/ApplicationDbScope.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.Sqlite;
+using SqlPlus;
+
+namespace SqlPlusApp.Data
+{
+    public class ApplicationDbScope : SqlScope<ApplicationDbScope, SqliteConnection, SqliteCommand, SqliteParameter>
+    {
+        public const string CONNECT_STRING = "filename=sqlplus.db";
+        public static ApplicationDbScope UseDefault() => new ApplicationDbScope(new SqliteConnection(CONNECT_STRING));
+
+        public ApplicationDbScope(SqliteConnection model) : base(model) { }
+    }
+
+}
---
+++ SqlPlusApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Northwnd;
+using NStandard;
+using SqlPlusApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace SqlPlusApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regions = new[]
+            {
+                QueryRegion_Traditional(1),
+                QueryRegion_SqlPlus(1),
+                QueryRegion_EF(1),
+            };
+            regions.Dump();
+
+            Console.WriteLine(regions.All(x => x == "Eastern"));
+        }
+
+        static string QueryRegion_Traditional(int regionId)
+        {
+            string ret;
+
+            var conn = new SqliteConnection("filename=northwnd.db");
+            conn.Open();
+
+            var cmd = new SqliteCommand("SELECT RegionDescription FROM Regions WHERE RegionId=@p0;", conn);
+            cmd.Parameters.Add(new SqliteParameter
+            {
+                ParameterName = "@p0",
+                Value = regionId,
+                DbType = DbType.Int32,
+            });
+
+            var reader = cmd.ExecuteReader();
+            reader.Read();
+            ret = reader.GetString(0);
+            reader.Close();
+
+            conn.Close();
+
+            return ret;
+        }
+
+        static string QueryRegion_SqlPlus(int regionId)
+        {
+            using (var sqlite = new ApplicationDbScope(new SqliteConnection("filename=northwnd.db")))
+            {
+                var region = sqlite.SqlQuery($"SELECT RegionDescription FROM Regions WHERE RegionId={regionId};").First();
+                return region["RegionDescription"] as string;
+            }
+        }
+
+        static string QueryRegion_EF(int regionId)
+        {
+            var options = new DbContextOptionsBuilder().UseSqlite("filename=northwnd.db").Options;
+            using (var context = new NorthwndContext(options))
+            {
+                return context.Regions.First(x => x.RegionID == regionId).RegionDescription;
+            }
+        }
+
+    }
+}
