d0398c5d3b21e46dc720088aa8f4442db11f6841
zmjack <jack@zmland.com>
2020-12-23 10:25:01 +0800

    v0.7.5

--- push.cmd
+++ push.cmd
@@ -1,4 +1,4 @@
-nuget push "SQLib/bin/Release/SQLib.0.7.4.nupkg" -source nuget.org
-nuget push "SQLib.MySQL/bin/Release/SQLib.MySQL.0.7.4.nupkg" -source nuget.org
-nuget push "SQLib.Sqlite/bin/Release/SQLib.Sqlite.0.7.4.nupkg" -source nuget.org
+nuget push "SQLib/bin/Release/SQLib.0.7.5.nupkg" -source nuget.org
+nuget push "SQLib.MySQL/bin/Release/SQLib.MySQL.0.7.5.nupkg" -source nuget.org
+nuget push "SQLib.Sqlite/bin/Release/SQLib.Sqlite.0.7.5.nupkg" -source nuget.org
 pause
