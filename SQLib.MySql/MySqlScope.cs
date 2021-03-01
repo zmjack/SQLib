96128cc6acd4b57da047adc1331d75785fc26614
zmjack <jack@zmland.com>
2021-03-01 14:00:06 +0800

    v0.8.0

---
+++ SQLib.MySql/MySqlScope.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace SQLib.MySQL
+{
+    public sealed class MySqlScope : MySqlScope<MySqlScope>
+    {
+        public MySqlScope(string connectionString) : this(new MySqlConnection(connectionString)) { }
+        public MySqlScope(MySqlConnection model) : base(model) { }
+    }
+
+    public class MySqlScope<TSelf> : SqlScope<TSelf, MySqlConnection, MySqlCommand, MySqlParameter>
+        where TSelf : MySqlScope<TSelf>
+    {
+        public MySqlScope(string connectionString) : this(new MySqlConnection(connectionString)) { }
+        public MySqlScope(MySqlConnection model) : base(model) { }
+    }
+}
