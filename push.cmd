9a8d2805348acb780a796ff213b7ef11898ac8f9
zmjack <jack@zmland.com>
2023-03-09 23:05:16 +0800

    SQLib.0.10.0

--- push.cmd
+++ push.cmd
@@ -1,5 +1,5 @@
-nuget push "SQLib/bin/Release/SQLib.0.9.0.nupkg" -source nuget.org
-nuget push "SQLib.MySql/bin/Release/SQLib.MySql.0.9.0.nupkg" -source nuget.org
-nuget push "SQLib.Sqlite/bin/Release/SQLib.Sqlite.0.9.0.nupkg" -source nuget.org
-nuget push "SQLib.SqlServer/bin/Release/SQLib.SqlServer.0.9.0.nupkg" -source nuget.org
+nuget push "SQLib/bin/Release/SQLib.0.10.0.nupkg" -source nuget.org
+nuget push "SQLib.MySql/bin/Release/SQLib.MySql.0.10.0.nupkg" -source nuget.org
+nuget push "SQLib.Sqlite/bin/Release/SQLib.Sqlite.0.10.0.nupkg" -source nuget.org
+nuget push "SQLib.SqlServer/bin/Release/SQLib.SqlServer.0.10.0.nupkg" -source nuget.org
 pause
